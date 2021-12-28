39f2e27df1b2be578376efd0b8bc1b14bbbcaf81
Mino7698 <91067163+Mino7698@users.noreply.github.com>
2021-12-28 20:02:16 +0300

    last commit 3

--- src/main/java/service/CredentialService.java
+++ src/main/java/service/CredentialService.java
@@ -10,31 +10,37 @@ import java.util.*;
 
 public class CredentialService {
 
-    public Map<String, List<List<Object>>> returnDifferenceOfCredentialsMap(String path1, String path2) throws IOException {
+    public Map<String, List<Object>> returnDifferenceOfCredentialsMap(String path1, String path2) throws IOException {
         Map<String, List<Object>> map1 = ReadJson.readJsonLikeAMap(path1);
         Map<String, List<Object>> map2 = ReadJson.readJsonLikeAMap(path2);
-        Map<String, List<Object>> sumMap = new HashMap<>(map1);
-        map2.entrySet().stream().forEach(entry -> {
-            sumMap.put(entry.getKey(), entry.getValue());
-        });
-        Set resultSet = Sets.symmetricDifference(map1.keySet(), map2.keySet());
+        Set<String> resultSet = Sets.symmetricDifference(map1.keySet(), map2.keySet());
 
-        MapDifference mapDifference = Maps.difference(map1,map2);
-        Map midResult = mapDifference.entriesDiffering();
-        Map result = new HashMap();
-        Set keys = midResult.keySet();
-        for (Object key : keys) {
+        MapDifference<String, List<Object>> mapDifference = Maps.difference(map1, map2);
+        Map<String, MapDifference.ValueDifference<List<Object>>> midResult = mapDifference.entriesDiffering();
+        Map<String,List<Object>> result = new HashMap<>();
+        Set<String> keys = midResult.keySet();
+        for (String key : keys) {
             List<Object> intersection = new ArrayList<>(map1.get(key));
             intersection.retainAll(map2.get(key));
             List<Object> differenceList = new ArrayList<>(map1.get(key));
             differenceList.removeAll(intersection);
-            result.put(key,differenceList);
+            result.put(key, differenceList);
         }
-        for (Object key : resultSet) {
+        for (String key : resultSet) {
             if (map1.containsKey(key)) {
-                result.put(key, map1.get(key));
+                if (!map1.get(key).isEmpty()) {
+                    if (((LinkedHashMap) map1.get(key).get(0)).size() != 0) {
+                        result.put(key, map1.get(key));
+                    }
+                }
             } else {
-                result.put(key, map2.get(key));
+                if (!map2.get(key).isEmpty()) {
+                    if (!map2.get(key).isEmpty()) {
+                        if (((LinkedHashMap) map2.get(key).get(0)).size() != 0) {
+                            result.put(key, map2.get(key));
+                        }
+                    }
+                }
             }
         }
 
@@ -43,8 +49,7 @@ public class CredentialService {
 
 
     public int returnNumberOfDifferenceOfCredentialsMap(String path1, String path2) throws IOException {
-        return returnDifferenceOfCredentialsMap(path1, path2).values()
-                .stream().mapToInt(List::size).sum();
+        return returnDifferenceOfCredentialsMap(path1, path2).values().stream().mapToInt(List::size).sum();
     }
 
 }
