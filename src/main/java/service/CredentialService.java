40b570e035c79803f5297f1f6e3b8ab8c698d74c
Mino7698 <91067163+Mino7698@users.noreply.github.com>
2021-12-28 15:46:06 +0300

    final commit (without generics)

--- src/main/java/service/CredentialService.java
+++ src/main/java/service/CredentialService.java
@@ -9,17 +9,17 @@ import java.util.*;
 
 public class CredentialService {
 
-    public Map returnDifferenceOfCredentialsMap (String path1, String path2) throws IOException {
-        Map map1 = ReadJson.readJsonLikeAMap(path1);
-        Map map2 = ReadJson.readJsonLikeAMap(path2);
+    public Map<String,List<List<Object>>> returnDifferenceOfCredentialsMap (String path1, String path2) throws IOException {
+        Map<String, List<Object>> map1 = ReadJson.readJsonLikeAMap(path1);
+        Map<String, List<Object>> map2 = ReadJson.readJsonLikeAMap(path2);
         MapDifference mapDifference = Maps.difference(map1,map2);
         Map midResult = mapDifference.entriesDiffering();
         Map result = new HashMap();
         Set keys = midResult.keySet();
         for (Object key : keys) {
-            List<Object> intersection = new ArrayList<>((List<Object>) map1.get(key));
-            intersection.retainAll((List<Object>) map2.get(key));
-            List<Object> differenceList = new ArrayList<>((List<Object>) map1.get(key));
+            List<Object> intersection = new ArrayList<>(map1.get(key));
+            intersection.retainAll(map2.get(key));
+            List<Object> differenceList = new ArrayList<>(map1.get(key));
             differenceList.removeAll(intersection);
             result.put(key,differenceList);
         }
@@ -28,14 +28,8 @@ public class CredentialService {
 
 
     public int returnNumberOfDifferenceOfCredentialsMap (String path1, String path2) throws IOException {
-        Map map = returnDifferenceOfCredentialsMap(path1,path2);
-        Set keys = map.keySet();
-        List<Object> differenceList = new ArrayList<>();
-        for (Object key : keys) {
-            differenceList.addAll((Collection<?>)map.get(key));
-        }
-
-        return differenceList.size();
+        return returnDifferenceOfCredentialsMap(path1, path2).values()
+                .stream().mapToInt(List::size).sum();
     }
 
 }
