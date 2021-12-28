5067acdec0365586d9da5079f458c992933b1cb5
Mino7698 <91067163+Mino7698@users.noreply.github.com>
2021-12-28 14:30:11 +0300

    delete some methods and tests

--- src/main/java/service/CredentialService.java
+++ src/main/java/service/CredentialService.java
@@ -9,25 +9,6 @@ import java.io.IOException;
 import java.util.*;
 
 public class CredentialService {
-    public List<ProfCredentials> returnDifference(String path1, String path2) throws IOException {
-        List<ProfCredentials> list1 = ReadJson.readJsonInClassWithCredentials(path1).getCredentials();
-        List<ProfCredentials> list2 = ReadJson.readJsonInClassWithCredentials(path2).getCredentials();
-        List<ProfCredentials> intersection = new ArrayList<>(list1);
-        intersection.retainAll(list2);
-        List<ProfCredentials> result = new ArrayList<>(list1);
-        result.removeAll(intersection);
-        return result;
-    }
-
-    public List<Object> returnDifferenceOfCorruptedCredentialsList (String path1, String path2) throws IOException {
-        List<Object> list1 = ReadJson.readJsonInClass(path1).getCredentials();
-        List<Object> list2 = ReadJson.readJsonInClass(path2).getCredentials();
-        List<Object> intersection = new ArrayList<>(list1);
-        intersection.retainAll(list2);
-        List<Object> result = new ArrayList<>(list1);
-        result.removeAll(intersection);
-        return result;
-    }
 
     public Map returnDifferenceOfCredentialsMap (String path1, String path2) throws IOException {
         Map map1 = ReadJson.readJsonLikeAMap(path1);
@@ -44,16 +25,6 @@ public class CredentialService {
             result.put(key,differenceList);
         }
         return result;
-    }
-
-
-
-    public int returnNumberOfDifference(String path1, String path2) throws IOException {
-        return returnDifference(path1,path2).size();
-    }
-
-    public int returnNumberOfDifferenceOfCorruptedCredentialsList (String path1, String path2) throws IOException {
-        return returnDifferenceOfCorruptedCredentialsList(path1,path2).size();
     }
 
     public int returnNumberOfDifferenceOfCredentialsMap (String path1, String path2) throws IOException {
--- src/test/java/CredentialServiceTest.java
+++ src/test/java/CredentialServiceTest.java
@@ -1,6 +1,4 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
-import model.Profession;
-import model.ProfessionWithListOfObjects;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,31 +17,8 @@ public class CredentialServiceTest {
     @Rule
     public ExpectedException exception = ExpectedException.none();
     CredentialService service = new CredentialService();
-    String path1 = "src/test/resources/Сredentials.json";
-    String path2 = "src/test/resources/AnotherСredentials.json";
-    String path3 = "src/test/resources/CorruptedСredentials.json";
-    String path5 = "src/test/resources/SourceCredentialsMap.json";
-    String path6 = "src/test/resources/ChangedCredentialsMap.json";
-
-    @Test
-    public void ServiceReturnDifferenceTest() throws IOException {
-        String ResultOfDifferencesOfCredentials = "src/test/resources/ResultDifferencesOfСredentials.json";
-
-        Profession resultDifferencesOfPrudentials
-                = new ObjectMapper().readValue(new File(ResultOfDifferencesOfCredentials), Profession.class);
-
-        Assert.assertEquals(resultDifferencesOfPrudentials.getCredentials(), service.returnDifference(path1, path2));
-    }
-
-    @Test
-    public void ServiceTestReturnDifferencesOfCredentialsList() throws IOException {
-        String pathResultDifferencesOfCorruptedCredentials = "src/test/resources/ResultDifferencesOfCorruptedCredentials.json";
-
-        ProfessionWithListOfObjects resultDifferencesOfCorruptedCredentials
-                = new ObjectMapper().readValue(new File(pathResultDifferencesOfCorruptedCredentials), ProfessionWithListOfObjects.class);
-
-        Assert.assertEquals(resultDifferencesOfCorruptedCredentials.getCredentials(), service.returnDifferenceOfCorruptedCredentialsList(path2, path3));
-    }
+    String path1 = "src/test/resources/SourceCredentialsMap.json";
+    String path2 = "src/test/resources/ChangedCredentialsMap.json";
 
     @Test
     public void ServiceTestReturnDifferencesOfCredentialsMap() throws IOException {
@@ -52,22 +27,12 @@ public class CredentialServiceTest {
         Map resultDifferencesOfCredentialsMap
                 = new ObjectMapper().readValue(new File(pathResultDifferencesOfCorruptedCredentials), Map.class);
 
-        Assert.assertEquals(resultDifferencesOfCredentialsMap, service.returnDifferenceOfCredentialsMap(path5, path6));
-    }
-
-    @Test
-    public void ServiceTestReturnNumberOfDifference() throws IOException {
-        Assert.assertEquals(3, service.returnNumberOfDifference(path1, path2));
-    }
-
-    @Test
-    public void ServiceTestReturnOfDifferencesOfCredentialsList() throws IOException {
-        Assert.assertEquals(3, service.returnNumberOfDifferenceOfCorruptedCredentialsList(path2, path3));
+        Assert.assertEquals(resultDifferencesOfCredentialsMap, service.returnDifferenceOfCredentialsMap(path1, path2));
     }
 
     @Test
     public void ServiceTestReturnNumberOfDifferenceOfCredentialsMap() throws IOException {
-        Assert.assertEquals(5, service.returnNumberOfDifferenceOfCredentialsMap(path5, path6));
+        Assert.assertEquals(5, service.returnNumberOfDifferenceOfCredentialsMap(path1, path2));
     }
 
 }
